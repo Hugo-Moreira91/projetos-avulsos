e85fac877b44de42d28537d030b44ba254bca050
Hugo-Moreira91 <hugo.cesar91@gmail.com>
2023-09-20 13:15:26 -0300

    Atualizando funcionalidades

--- age-calculator-app/funtions/age-validation.js
+++ age-calculator-app/funtions/age-validation.js
@@ -1,5 +1,4 @@
-
-function calcAge() {
+document.querySelector('form').addEventListener('submit', function (e){
     // Obter os valores dos campos de entrada
     var day = parseInt(document.getElementById("iday").value);
     var month = parseInt(document.getElementById("imonth").value);
@@ -32,12 +31,6 @@ function calcAge() {
         if(isNaN(year)){
             errorYear.innerHTML = 'This field is required';
         }
-        /*
-        dayResult.innerHTML = '--';
-        monthResult.innerHTML = '--';
-        yearResult.innerHTML = '--';
-        return;
-        */
     }else{
         // Com todos os campos - dia, mês e ano - preenchidos, vamos verificar se são válidos...
         // Verificando primeiro o ano
@@ -46,10 +39,12 @@ function calcAge() {
         }else if(year > currentYear){
             errorYear.innerHTML = 'Must be in the past';
         }else{
+            errorYear.innerHTML = "";
             // Verificando o mês
             if(month < 1 || month > 12){
                 errorMonth.innerHTML = 'Must be a valid month';
             }else{
+                errorMonth.innerHTML = "";
                 // Verificando o dia
                 // Verificar anos bissextos
                 function isLeapYear(year) {
@@ -62,6 +57,7 @@ function calcAge() {
                 if(day < 1 || day > daysPerMonth[month - 1]){
                     errorDay.innerHTML = 'Must be a valid day';
                 }else{
+                    errorDay.innerHTML = "";
                     // Com a data validada, podemos calcular a idade da pessoa...
                     var yearsAge = currentYear - year;
                     var monthsAge = currentMonth - month;
@@ -85,128 +81,27 @@ function calcAge() {
 
                     return;
                 }
+                dayResult.innerHTML = '--';
+                monthResult.innerHTML = '--';
+                yearResult.innerHTML = '--';
+                e.preventDefault();
+                return;
             }
+            dayResult.innerHTML = '--';
+            monthResult.innerHTML = '--';
+            yearResult.innerHTML = '--';
+            e.preventDefault();
+            return;
         }
+        dayResult.innerHTML = '--';
+        monthResult.innerHTML = '--';
+        yearResult.innerHTML = '--';
+        e.preventDefault();
+        return;
     }
     dayResult.innerHTML = '--';
     monthResult.innerHTML = '--';
     yearResult.innerHTML = '--';
+    e.preventDefault();
     return;
-}
-    
-
-
-/*
-document.querySelector('form').addEventListener('submit', function (e){
-    const day = parseInt(document.getElementById('iday').value);
-    const month = parseInt(document.getElementById('imonth').value);
-    const adjustMonth = parseInt(document.getElementById('imonth').value) - 1;
-    const year = parseInt(document.getElementById('iyear').value);
-
-    var dayResult = document.getElementById('day-result');
-    var monthResult = document.getElementById('month-result');
-    var yearResult = document.getElementById('year-result');
-
-    const currentYear = new Date().getFullYear();
-    const currentDate = new Date();
-
-    const validDays = [31, 28 + (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-    var msgDay = document.getElementById('day-error');
-    var msgMonth = document.getElementById('month-error');
-    var msgYear = document.getElementById('year-error');
-
-    let birthYears = currentDate.getFullYear() - year;
-    let birthMonths = currentDate.getMonth() - adjustMonth;
-    let birthDays = currentDate.getDay() - day;
-
-    if(day === ""){
-        msgDay.innerHTML = 'This field is required';
-        yearResult.innerHTML = '--';
-        monthResult.innerHTML = '--';
-        dayResult.innerHTML = '--';
-        e.preventDefault();
-        return;
-    }else{
-        msgDay.style.display = 'none';
-    }
-    if(month === ""){
-        msgMonth.innerHTML = 'This field is required';
-        yearResult.innerHTML = '--';
-        monthResult.innerHTML = '--';
-        dayResult.innerHTML = '--';
-        e.preventDefault();
-        return;
-    }else{
-        msgMonth.style.display = 'none';
-    }
-    if(year === ""){
-        msgYear.innerHTML = 'This field is required';
-        yearResult.innerHTML = '--';
-        monthResult.innerHTML = '--';
-        dayResult.innerHTML = '--';
-        e.preventDefault();
-        return;
-    }else{
-        msgYear.style.display = 'none';
-    }
-
-    if(isNaN(month) || month < 1 || month > 12){
-        msgMonth.innerHTML = 'Must be a valid month';
-        yearResult.innerHTML = '--';
-        monthResult.innerHTML = '--';
-        dayResult.innerHTML = '--';
-        e.preventDefault();
-        return;
-    }else{
-        msgMonth.style.display = 'none';
-    }
-
-    if(isNaN(year) || year.toString().length !== 4){
-        msgYear.innerHTML = 'Must be a valid year';
-        yearResult.innerHTML = '--';
-        monthResult.innerHTML = '--';
-        dayResult.innerHTML = '--';
-        e.preventDefault();
-        return;
-    }else if(year > currentYear){
-        msgYear.innerHTML = 'Must be in the past';
-        yearResult.innerHTML = '--';
-        monthResult.innerHTML = '--';
-        dayResult.innerHTML = '--';
-        e.preventDefault();
-        return;
-    }else{
-        msgYear.style.display = 'none';
-    }
-
-    if(isNaN(day) || day < 1 || day > validDays[month - 1]){
-        msgDay.innerHTML = 'Must be a valid day';
-        yearResult.innerHTML = '--';
-        monthResult.innerHTML = '--';
-        dayResult.innerHTML = '--';
-        e.preventDefault();
-        return;
-    }else{
-        msgDay.style.display = 'none';
-    }
-
-    if(birthDays < 0){
-        birthMonths--;
-        birthDays += validDays[currentDate.getMonth() - 1];
-    }
-
-    if(birthMonths < 0){
-        birthYears--;
-        birthMonths += 12;
-    }
-
-    yearResult.innerHTML = birthYears;
-    monthResult.innerHTML = birthMonths;
-    dayResult.innerHTML = birthDays;
-
-    return;
-
 });
-
-*/
