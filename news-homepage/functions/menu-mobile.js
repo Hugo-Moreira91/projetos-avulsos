e9761ff56bfbdb1db6d374c5eb8f8d4d42c2eb75
Hugo-Moreira91 <hugo.cesar91@gmail.com>
2023-09-22 15:42:30 -0300

    Projeto finalizado

---
+++ news-homepage/functions/menu-mobile.js
@@ -0,0 +1,13 @@
+function clickMenu(){
+
+    var menuHamburguer = document.getElementById('menu-itens');
+
+    menuHamburguer.style.display = 'block';
+}
+
+function closeMenu(){
+
+    var menuHamburguer = document.getElementById('menu-itens');
+
+    menuHamburguer.style.display = 'none';
+}
